deb149f001b7652a7db0fdf8960713ea91ce3ee0
NicolasSolans <nico.solans.drc@gmail.com>
2023-07-25 15:53:04 -0300

    nueva variable

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 // Obtener el elemento <h1> del DOM
-const saludoElemento = document.getElementById('saludo');
+const saludoElemento = document.getElementById("saludo");
 
 // Establecer el mensaje "Hola Mundo" en el elemento <h1>
-saludoElemento.textContent = '¡Hola Mundo!';
+saludoElemento.textContent = "¡Hola Mundo!";
 
-saludoElemento.textContent += '';
+saludoElemento.textContent += "";
 
-const prueba1 = document.getElementById('prueba1');
+const prueba1 = document.getElementById("prueba1");
 
 const prueba2 = "hola";
+
+const nuevaVariable = "variable de Nicolas";
